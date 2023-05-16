53160a75a282521fe59fca0ee759bbd8d0e95b4a
WendelGregorio <contact.wendelg@gmail.com>
2023-05-16 10:38:35 -0300

    return default project start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@/components/Button";
+
 
 export default function Home() {
   return (
     <h1>
       Hello World!
-      <Button title="Teste"/>
-      <Button title="Teste2"/>
-      <Button title="Teste3"/>
     </h1>
   )
 }
