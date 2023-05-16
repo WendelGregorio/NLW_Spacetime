1c3f9fdd65cb79bad32f77ef81044ca428983459
WendelGregorio <contact.wendelg@gmail.com>
2023-05-16 10:08:43 -0300

    add and init prisma

--- src/server.ts
+++ src/server.ts
@@ -11,5 +11,5 @@ app
     port: 3333,
   })
   .then(() => {
-    console.log('HTTP server running on http://localhost:3333')
+    console.log('🚀 HTTP server running on http://localhost:3333')
   })
