1b292ababc5d97ed6340e5c826e1534dfee9bfa0
WendelGregorio <contact.wendelg@gmail.com>
2023-05-16 10:05:49 -0300

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
-app.listen({
-    port: 3333,
-}).then(() => {
-    console.log('HTTP server running on http://localhost:3333')
+app.get('/hello', () => {
+  return 'Hello World'
 })
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
