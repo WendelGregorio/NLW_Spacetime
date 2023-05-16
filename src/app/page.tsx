97f0db84625d79e667b3799dc0c2d52442d6d65c
WendelGregorio <contact.wendelg@gmail.com>
2023-05-16 10:37:24 -0300

    test component Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/Button";
+
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <h1>
+      Hello World!
+      <Button title="Teste"/>
+      <Button title="Teste2"/>
+      <Button title="Teste3"/>
+    </h1>
   )
 }
