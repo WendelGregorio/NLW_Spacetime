f718e99ff490db76fd3afeb5e31129bcff00c45d
WendelGregorio <contact.wendelg@gmail.com>
2023-05-17 08:53:59 -0300

    feat: prisma config, create routes for memories

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.register(cors, {
+  origin: true,
 })
+app.register(memoriesRoutes)
 
 app
   .listen({
